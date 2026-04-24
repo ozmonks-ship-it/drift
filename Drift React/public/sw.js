3ae571e79dd76d687b6baeea05cd46f90e2aab4b
Ozgur <ozgurmonkul@Ozgurs-iMac.local>
2026-04-24 21:02:35 +1000

    fix service worked to avoid cache refresh needs

--- Drift React/public/sw.js
+++ Drift React/public/sw.js
@@ -1,14 +1,21 @@
 const CACHE_NAME = 'drift-v1';
-const URLS_TO_CACHE = ['/'];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
   );
 });
